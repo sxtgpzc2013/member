986da16abafdffce695f9999c58ab704cf830a89
lamproot <304151978@qq.com>
2016-09-16 16:18:58 +0800

    测试提交

---
+++ cms/Lib/Model/BounsModel.class.php
@@ -0,0 +1,17 @@
+<?php if (!defined('THINK_PATH')) exit();//判断是否加载thinkphp,如果否则退出
+/*********文件描述*********
+ * @last update 2014-09-15
+ * @alter 张睿
+ * @version 1.0.0
+ *
+ * 功能简介：商户后台订单MODEL类
+ * @author 张睿
+ * @copyright 张睿
+ * @time 2014-09-15
+ * @version 1.0.0
+ */
+
+	class BonusModel extends CommonModel
+	{
+
+	}
