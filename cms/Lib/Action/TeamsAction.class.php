f04d620d9f64f22046f6c3e0002beb855600abdd
lamproot <304151978@qq.com>
2016-09-16 16:23:44 +0800

    测试提交

--- cms/Lib/Action/TeamsAction.class.php
+++ cms/Lib/Action/TeamsAction.class.php
@@ -1,13 +1,13 @@
 <?php if (!defined('THINK_PATH')) exit();//判断是否加载thinkphp,如果否则退出
 /*********文件描述*********
- * @last update 2014-06-12
- * @alter 张睿
+ * @last
+ * @alter
  * @version 1.0.0
  *
- * 功能简介：商户后台首页控制器类
- * @author 张睿
+ * 功能简介：
+ * @author
  * @copyright
- * @time 2014-06-12
+ * @time
  * @version 1.0.0
  */
 	class TeamsAction extends CommonAction {
@@ -41,6 +41,6 @@
 		 */
 	    public function index()
 	    {
-
+			$this->display();
 	    }
 	}
