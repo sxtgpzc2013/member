e687b248fe862794469be52c64068307077fdea3
pangzecheng <pangzecheng@6clue.com>
2016-09-17 17:42:54 +0800

    消费商转币 完成

--- index/Lib/Action/FinancesAction.class.php
+++ index/Lib/Action/FinancesAction.class.php
@@ -119,7 +119,7 @@
 	    }
 
 	    /**
-		 * 首页
+		 * 提现记录
 		 *
 		 * 参数描述：
 		 *
@@ -142,5 +142,145 @@
 	    	$this -> assign('withdrawals', $withdrawals);
 
 			$this -> display();
+	    }
+
+	    /**
+		 * 消费商转币
+		 *
+		 * 参数描述：
+		 *
+		 *
+		 *
+		 * 返回值：
+		 *
+		 */
+	    public function transfer()
+	    {
+	    	$params = array(
+
+	    		'table_name' => 'member',
+
+	    		'where' => "uid = {$_SESSION['Rongzi']['user']['uid']}"
+	    	);
+
+	    	$member = $this -> model -> my_find($params);
+
+	    	if (isset($_POST['form_key']) && $_POST['form_key'] == 'yes')
+	    	{
+	    		$targetusernumber = isset($_POST['targetusernumber']) && intval($_POST['targetusernumber']) != $member['usernumber'] ? intval($_POST['targetusernumber']) : $this -> _back('非法的转入用户编号');
+
+	    		$money = isset($_POST['money']) && floatval($_POST['money']) > 0 ? floatval($_POST['money']) : $this -> _back('非法的转币数量');
+
+	    		if ($member['baodanbi'] - $money < 0)
+	    		{
+	    			$this -> _back('余额不足');
+	    		}
+
+	    		//查询目标用户
+	    		$params = array(
+
+	    			'table_name' => 'member',
+
+	    			'where' => "usernumber = {$targetusernumber} AND status = 1"
+	    		);
+
+	    		$target_member = $this -> model -> my_find($params);
+
+	    		if (!$target_member)
+	    		{
+	    			$this -> _back('非法的转入用户编号');
+	    		}
+
+	    		//扣除当前用户数值
+	    		$params = array(
+
+	    			'table_name' => 'member',
+
+	    			'where' => "uid = {$member['uid']}",
+
+	    			'data' => array(
+
+	    				'baodanbi' => $member['baodanbi'] - $money
+	    			)
+	    		);
+
+	    		$member_model = M('member');
+
+	    		//开启事务
+	    		$member_model -> startTrans();
+
+	    		$member_save = $this -> model -> my_save($params);
+
+	    		if (!$member_save)
+	    		{
+	    			$member_model -> rollback();
+
+	    			$this -> _back('操作失败 请稍后重试');
+	    		}
+
+	    		//修改对方的余额
+	    		$params = array(
+
+	    			'table_name' => 'member',
+
+	    			'where' => "uid = {$target_member['uid']}",
+
+	    			'data' => array(
+
+	    				'baodanbi' => $target_member['baodanbi'] + $money
+	    			)
+	    		);
+
+	    		$target_member_save = $this -> model -> my_save($params);
+
+	    		if ($target_member_save)
+	    		{
+	    			$member_model -> commit();
+
+	    			$data['status'] = 0;
+	    		}
+	    		else
+	    		{
+	    			$member_model -> rollback();
+
+	    			$data['status'] = 1;
+	    		}
+
+	    		$data['userid'] = $member['uid'];
+
+	    		$data['usernumber'] = $member['usernumber'];
+
+	    		$data['targetuserid'] = $target_member['uid'];
+
+	    		$data['targetusernumber'] = $target_member['usernumber'];
+
+	    		$data['moneytype'] = 0;
+
+	    		$data['money'] = $money;
+
+	    		$data['createtime'] = time();
+
+	    		$params = array(
+
+	    			'table_name' => 'transfer',
+
+	    			'data' => $data
+	    		);
+
+	    		$transfer_add = $this -> model -> my_add($params);
+
+	    		if ($transfer_add)
+	    		{
+	    			redirect(__APP__.'/Finances/transfer', 0);
+	    		}
+	    		else
+	    		{
+	    			$this -> _back('记录保存失败 请稍后重试');
+	    		}
+	    	}
+
+	    	$this -> assign('member', $member);
+
+	    	$this -> display();
 	    }
 	}
