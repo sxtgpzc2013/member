97f4995b1a5e87afeb5ab988be827a1dc08335b6
pangzecheng <pangzecheng@6clue.com>
2016-09-21 17:14:34 +0800

    公司扣币 完成

--- admin/Lib/Action/FinancesAction.class.php
+++ admin/Lib/Action/FinancesAction.class.php
@@ -270,6 +270,171 @@ class FinancesAction extends CommonAction {
     }
 
     /**
+	 * 公司扣币
+	 *
+	 * 参数描述：
+	 *
+	 *
+	 *
+	 * 返回值：
+	 *
+	 */
+    public function company_deduct_money()
+    {
+    	if (isset($_POST['form_key']) && htmlspecialchars($_POST['form_key']) == 'yes')
+    	{
+    		$usernumber = isset($_POST['usernumber']) ? intval($_POST['usernumber']) : $this -> _back('非法的消费商');
+
+    		$money = isset($_POST['money']) ? floatval($_POST['money']) : $this -> _back('非法的金额');
+
+    		$type = isset($_POST['type']) ? intval($_POST['type']) : $this -> _back('非法的类型');
+
+    		//查询目标用户
+    		$params = array(
+
+    			'table_name' => 'member',
+
+    			'where' => "usernumber = {$usernumber}"
+    		);
+
+    		$member_find = $this -> model -> my_find($params);
+
+    		if (!$member_find)
+    		{
+    			$this -> _back('无效的消费商');
+    		}
+
+    		//查询当前登陆者
+    		$params = array(
+
+    			'table_name' => 'member',
+
+    			'where' => "uid = {$_SESSION['Rongzi']['admin']['id']}"
+    		);
+
+    		$user_find = $this -> model -> my_find($params);
+
+    		if (!$user_find)
+    		{
+    			$this -> _back('请先登录');
+    		}
+
+    		switch ($type)
+    		{
+    			case 1 : //奖金币
+
+    				$data['jiangjinbi'] = $member_find['jiangjinbi'] - $money;
+
+    				$balance = $data['jiangjinbi'];
+
+    				$moneytype = 6;
+
+    				break;
+
+    			case 2 : //报单币
+
+    				$data['baodanbi'] = $member_find['baodanbi'] - $money;
+
+    				$balance = $data['baodanbi'];
+
+    				$moneytype = 2;
+
+    				break;
+
+    			case 3 : //激活币
+
+    				$data['jihuobi'] = $member_find['jihuobi'] - $money;
+
+    				$balance = $data['jihuobi'];
+
+    				$moneytype = 4;
+
+    				break;
+
+    			case 4 : //戎子盾
+
+    				$data['rongzidun'] = $member_find['rongzidun'] - $money;
+
+    				$balance = $data['rongzidun'];
+
+    				$moneytype = 3;
+
+    				break;
+
+    			case 5 : //奖励积分
+
+    				$data['jianglijifen'] = $member_find['jianglijifen'] - $money;
+
+    				$balance = $data['jianglijifen'];
+
+    				$moneytype = 5;
+
+    				break;
+    		}
+
+    		$data['update_time'] = time();
+
+    		$params = array(
+
+    			'table_name' => 'member',
+
+    			'where' => "usernumber = {$usernumber}",
+
+    			'data' => $data
+    		);
+
+    		$member_save = $this -> model -> my_save($params);
+
+    		$money_change_data['moneytype'] = $moneytype;
+    			
+			$money_change_data['status'] = $member_save ? 1 : 0;
+			
+			$money_change_data['targetuserid'] = $member_find['uid'];
+			
+			$money_change_data['targetusernumber'] = $member_find['usernumber'];
+			
+			$money_change_data['targetrealname'] = $member_find['realname'];
+			
+			$money_change_data['userid'] = $user_find['uid'];
+			
+			$money_change_data['usernumber'] = $user_find['usernumber'];
+			
+			$money_change_data['realname'] = $user_find['realname'];
+			
+			$money_change_data['changetype'] = 2;
+			
+			$money_change_data['recordtype'] = 0;
+			
+			$money_change_data['money'] = $money;
+			
+			$money_change_data['hasmoney'] = $balance;
+			
+			$money_change_data['createtime'] = time();
+
+			//存入流水
+			$params = array(
+
+				'table_name' => 'money_change',
+
+				'data' => $money_change_data
+			);
+
+			$money_change_add = $this -> model -> my_add($params);
+
+    		if ($member_save)
+    		{
+    			redirect(__APP__.'/Finances/finance_flow', 0);
+    		}
+    		else
+    		{
+    			$this -> _back('扣币失败 请重试');
+    		}
+    	}
+
+    	$this -> display();
+    }
+
+    /**
 	 * 充值记录
 	 *
 	 * 参数描述：
