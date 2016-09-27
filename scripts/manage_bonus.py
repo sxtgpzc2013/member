a01a1658515e4c99ce36f70be25c77e775cf251e
litong <litong@6clue.com>
2016-09-27 09:50:47 +0800

    user logo

--- scripts/manage_bonus.py
+++ scripts/manage_bonus.py
@@ -20,22 +20,24 @@ def managerbonus():
 
 	members = conn.query(member_sql)
 
-	print members
-
-	if members:
-		for member in members:
-			leftachievement = member['leftachievement']
-			middleachievement = member['middleachievement']
-			rightachievement = member['rightachievement']
-
-			print leftachievement, middleachievement, rightachievement
-			
 	# 查看这些会员的 最小2区的业绩 达到了 多少, 判断 现有的 会员头衔
 
 	# 3个区 取最小的2个区， 如果大于 设定的值, update 会员头衔 add 奖励积分， 更具激活时间 计算管理奖， 管理奖必须有推荐关系，滑落的点不计算管理奖， 管理奖是极差制度
 
 	#互助奖，享受管理补贴的代数的奖励
 
+	print members
+
+	if members:
+		for member in members:
+			member_leftachievement = {}
+			member_leftachievement['leftachievement'] = member['leftachievement']
+			member_leftachievement['middleachievement'] = member['middleachievement']
+			member_leftachievement['rightachievement'] = member['rightachievement']
+
+			print member_leftachievement
+
+	
 def leaderbonus():
 	pass
 
