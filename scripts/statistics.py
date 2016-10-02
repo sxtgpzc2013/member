43dba10fbfbebf8485bbefe662c048ec76665f7a
litong <litong@6clue.com>
2016-10-03 00:56:55 +0800

    管理补贴算法

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -11,7 +11,6 @@ if sys.getdefaultencoding() != default_encoding:
 
 conn = mysql.db()
 now = datetime.datetime.now()
-now_time = datetime.datetime.now().strftime('%s')
 yes_time = (now + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')
 
 def money_change(uid, usernumber):
@@ -57,7 +56,7 @@ def money_change(uid, usernumber):
 
 	zx_bonus_count_sql = """
 		insert into zx_bonus_count (touserid, tousernumber, bonus1, bonus2, bonus3, bonus4, bonus5, bonus6, bonus7, bonus8, total, real_total, count_date) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-	""" % (uid, usernumber, fenhong_total, manager_total, leader_total, expand_total, market_total, consume_total, service_total, twice_consume_total, total, realtotal, now_time)
+	""" % (uid, usernumber, fenhong_total, manager_total, leader_total, expand_total, market_total, consume_total, service_total, twice_consume_total, total, realtotal, yes_time)
 	conn.dml(zx_bonus_count_sql, 'insert')
 
 def main():
