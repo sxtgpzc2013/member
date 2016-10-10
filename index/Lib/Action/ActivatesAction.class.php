d95278c7801ad6021a4bc5bc22a8e87db8f8f3d5
quanboxiong <quanboxiong@6clue.com>
2016-10-10 12:28:58 +0800

    更新拓展相关

--- index/Lib/Action/ActivatesAction.class.php
+++ index/Lib/Action/ActivatesAction.class.php
@@ -738,7 +738,7 @@ class ActivatesAction extends CommonAction {
 			unset($expand[$admin_offset]);
 		}
 
-		$expand_slice = array_slice($expand, 0, 1);
+		$expand_slice = array_slice($expand, 0, 3);
 
 		//处理几级拓展补贴
 		foreach ($expand_slice as $key => $value) {
