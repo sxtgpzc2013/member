8fb16e37e09a483c32bea8ac1884820e133d8367
litong <litong@6clue.com>
2016-10-10 10:28:48 +0800

    管理补贴算法

---
+++ scripts/consum_bonus.py
@@ -0,0 +1,20 @@
+#encoding:utf-8
+
+import mysql
+import datetime
+import sys
+
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+
+conn = mysql.db()
+now = datetime.datetime.now()
+now_second = datetime.datetime.now().strftime('%s')
+
+def main():
+	pass
+
+if __name__ == '':
+	main()
