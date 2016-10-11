864f222801cfa3b1a06113287ec5d3608268c444
quanboxiong <quanboxiong@6clue.com>
2016-10-11 19:25:44 +0800

    更新消费商相关

--- admin/Lib/Action/SetAction.class.php
+++ admin/Lib/Action/SetAction.class.php
@@ -1,13 +1,13 @@
 <?php if (!defined('THINK_PATH')) exit();//判断是否加载thinkphp,如果否则退出
 /*********文件描述*********
- * @last update  
+ * @last update
  * @alter
  * @version 1.0.0
  *
  * 功能简介：
- * @author 
- * @copyright 
- * @time 
+ * @author
+ * @copyright
+ * @time
  * @version 1.0.0
  */
 	class SetAction extends CommonAction {
@@ -41,9 +41,48 @@
 		 * 返回值：
 		 *
 		 */
-	    public function index()
+	    public function bonus()
 	    {
+
+			//获取奖金相关设置数据
+	    	$params = array(
+
+	    		'table_name' => 'bonus_rule',
+
+	    		'where' => "1"
+    		);
+
+    		$result = $this -> model -> easy_select($params);
+
+    		$this -> assign('result', $result);
+
 	    	$this -> display();
+	    }
+
+
+		//修改奖金相关设置
+		public function editbonus()
+	    {
+
+			foreach ($_POST as $key => $value) {
+
+				$data['value'] = $value;
+
+				//获取奖金相关设置数据
+		    	$params = array(
+
+		    		'table_name' => 'bonus_rule',
+
+		    		'where' => "id = ". $key,
+
+					'data' => $data
+	    		);
+
+	    		$save = $this -> model -> my_save($params);
+			}
+
+
+    		redirect(__APP__."/Set/bonus", 0);
 	    }
 
 	}
