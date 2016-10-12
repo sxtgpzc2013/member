d2441e783a50ad9e34f80121157e0d3e887ab7c4
litong <litong@6clue.com>
2016-10-12 16:25:54 +0800

    管理补贴算法

---
+++ scripts/main.py
@@ -0,0 +1,24 @@
+#encoding:utf-8
+
+import datetime
+import sys
+import subprocess
+
+
+
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+
+
+def main():
+	cmd1 = "python manage_bonus.py"
+	info1 = subprocess.Popen(cmd1, stdout = subprocess.PIPE, shell = True).communicate()[0].strip()
+	if info1 == "ok":
+		cmd2 = "python achievement.py"
+		info2 = subprocess.Popen(cmd1, stdout = subprocess.PIPE, shell = True).communicate()[0].strip()
+		print info2
+
+if __name__ == '__main__':
+	main()
