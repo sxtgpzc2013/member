66b341f55fa8ace48b489a7b31b60f29583559fc
litong <litong@6clue.com>
2016-10-27 13:39:59 +0800

    管理补贴算法

--- scripts/upgrade.py
+++ scripts/upgrade.py
@@ -8,8 +8,10 @@ default_encoding = 'utf-8'
 if sys.getdefaultencoding() != default_encoding:
     reload(sys)
     sys.setdefaultencoding(default_encoding)
-
+    
 conn = mysql.db()
+now = datetime.datetime.now()
+now_second = datetime.datetime.now().strftime('%s')
 
 def rate():
 	rate_sql = """
