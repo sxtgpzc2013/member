cdff32097b38bec73aecc08413494b5ee551a461
pangzecheng <pangzecheng@6clue.com>
2016-10-28 16:22:50 +0800

    后台财务流水默认今天

--- admin/Lib/Action/FinancesAction.class.php
+++ admin/Lib/Action/FinancesAction.class.php
@@ -220,7 +220,7 @@ class FinancesAction extends CommonAction {
     public function finance_flow()
     {
         //默认导出今天数据
-        $start = $_GET['start'] ? strtotime($_GET['start']) : time();
+        $start = $_GET['start'] ? strtotime($_GET['start']) : strtotime(date('Y-m-d', time()));
 
         $stop = $_GET['stop'] ? strtotime($_GET['stop']) + 24 * 60 * 60 : time() + 24 * 60 * 60 ;
 
