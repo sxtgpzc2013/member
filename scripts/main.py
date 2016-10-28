98d3d06e36424fcc13dd416268fd7556819d637b
litong <litong@6clue.com>
2016-10-28 11:27:50 +0800

    管理补贴算法

--- scripts/main.py
+++ scripts/main.py
@@ -9,13 +9,34 @@ if sys.getdefaultencoding() != default_encoding:
     reload(sys)
     sys.setdefaultencoding(default_encoding)
 
+conn = mysql.db()
+
+# usernubmer, mobile
+def getmemberinfo(uid):
+	flag = False
+	sql = """
+		select usernumber, mobile from zx_member where uid = %s
+	""" % (uid)
+	result = conn.query(sql)
+	if result:
+		return result
+
+	return flag
+
 def main(uid):
 	cmd1 = "python /var/www/member/scripts/manage_bonus.py %s" % (uid)
 	info1 = subprocess.Popen(cmd1, stdout = subprocess.PIPE, shell = True).communicate()[0].strip()
 	if info1 == "ok":
 		cmd2 = "python /var/www/member/scripts/achievement.py"
 		info2 = subprocess.Popen(cmd2, stdout = subprocess.PIPE, shell = True).communicate()[0].strip()
-		print info2
+		if info2 == "ok":
+				result = getmemberinfo(uid)
+				if result:
+					usernumber = result[0]['usernumber'] 
+					mobile = result[0]['mobile']
+					cmd3 = "python /var/www/member/scripts/sms.py %s %s" % (usernumber, mobile)
+					info3 = subprocess.Popen(cmd3, stdout = subprocess.PIPE, shell = True).communicate()[0].strip()
+					print "ok"
 
 if __name__ == '__main__':
 	if len(sys.argv) >= 2:
